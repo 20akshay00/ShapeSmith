99fa24fbfe78bc0fe0392c84806f6b7667345b19
Akshay <sakshays.2000@gmail.com>
2024-08-17 21:55:52 +0200

    some bug fixes

--- scripts/grid/grid.gd
+++ scripts/grid/grid.gd
@@ -2,11 +2,21 @@
 class_name Grid
 extends Node2D
 
+@export var LINE_THICKNESS: int = 2
+@export var LINE_COLOR: Color = Color.WHITE
+
+@export var POINT_SIZE: int = 2
+@export var POINT_COLOR: Color = Color.AQUA
+
+@export var POINT_SELECTED_SIZE: int = 4
+@export var POINT_SELECTED_COLOR: Color = Color.WHITE
+
 @export var grid_size: Vector2 = Vector2(8, 8)
 @export var cell_size: Vector2 = Vector2(48, 48)
 @export var POINT_CLICK_SENSITIVITY: float = 15.
 
 @onready var _state_machine: GridStateMachine = $StateMachine
+var _points_default: Array[Vector2]
 var _points: Array[Vector2]
 var _show_focus: bool = true
 
@@ -18,7 +28,8 @@ func _ready() -> void:
 	for i in range(-grid_size.x/2, grid_size.x/2 + 1):
 		for j in range(-grid_size.y/2, grid_size.y/2 + 1):
 			_points.push_back(Vector2(i, j) * cell_size)
-			
+	
+	_points_default = _points.duplicate()
 	_state_machine.init()
 
 func _draw() -> void:
@@ -26,16 +37,19 @@ func _draw() -> void:
 	_focus_idx = -1
 	
 	for idx in len(_shape_idx) - 1:
-		draw_line(_points[_shape_idx[idx]], _points[_shape_idx[idx + 1]], Color.WHITE, 1)
-	if _shape_complete: draw_line(_points[_shape_idx[0]], _points[_shape_idx[-1]], Color.WHITE, 1)
+		draw_line(_points[_shape_idx[idx]], _points[_shape_idx[idx + 1]], LINE_COLOR, LINE_THICKNESS)
+	if _shape_complete: draw_line(_points[_shape_idx[0]], _points[_shape_idx[-1]], LINE_COLOR, LINE_THICKNESS)
 
 	for idx in len(_points):
 		var mouse_pos: Vector2 = get_local_mouse_position()	
 		if _show_focus and (mouse_pos - _points[idx]).length() < POINT_CLICK_SENSITIVITY:
-			draw_circle(_points[idx], 4, Color.WHITE)
+			draw_circle(_points[idx], POINT_SELECTED_SIZE, POINT_SELECTED_COLOR)
 			_focus_idx = idx
 		else:
-			draw_circle(_points[idx], 2, Color.AQUA)
+			draw_circle(_points[idx], POINT_SIZE, POINT_COLOR)
+
+		if not _shape_complete and len(_shape_idx) > 0: draw_circle(_points[_shape_idx[-1]], POINT_SELECTED_SIZE, POINT_SELECTED_COLOR)
+
 func _process(delta: float) -> void:
 	queue_redraw()
 
--- scripts/grid/grid_draw_state.gd
+++ scripts/grid/grid_draw_state.gd
@@ -12,8 +12,8 @@ func exit() -> void:
 	pass
 
 func on_draw() -> void:
-	grid.draw_circle(grid._points[_start_point_idx], 4, Color.WHITE)
-	grid.draw_line(grid._points[_start_point_idx], grid.get_local_mouse_position(), Color.WHITE, 1)
+	grid.draw_circle(grid._points[_start_point_idx], grid.POINT_SELECTED_SIZE, grid.POINT_SELECTED_COLOR)
+	grid.draw_line(grid._points[_start_point_idx], grid.get_local_mouse_position(), grid.LINE_COLOR, grid.LINE_THICKNESS)
 
 func on_input(event: InputEvent) -> void:
 	if event.is_action_pressed("draw") and grid._focus_idx > 0:
@@ -23,13 +23,28 @@ func on_input(event: InputEvent) -> void:
 			if len(grid._shape_idx) == 0: grid._shape_idx.push_back(_start_point_idx)
 			
 			if _next_point_idx == grid._shape_idx[0]:
+				#if _is_self_intersecting(grid._shape_idx.map(func(idx): return grid._points[idx])):
+					#grid._shape_idx = []
+					#transition_requested.emit(self, State.BASE)
+				#else:
 				grid._shape_complete = true
 				transition_requested.emit(self, State.BASE)
-			else:
+					
+			elif _next_point_idx not in grid._shape_idx:
 				grid._shape_idx.push_back(_next_point_idx)
 				_start_point_idx = _next_point_idx
-			
-			#transition_requested.emit(self, State.BASE)
 
 	if event.is_action_pressed("cancel"):
 		transition_requested.emit(self, State.BASE)
+
+func _is_self_intersecting(polygon: Array) -> bool:
+	var s = polygon.size() - 1
+	for i in range(0, s):
+		var p1: Vector2 = polygon[i]
+		var p2: Vector2 = polygon[(i+1) % s]
+		for j in range(0, s):
+			var p1a: Vector2 = polygon[j]
+			var p2a: Vector2 = polygon[(j+1) % s]
+			var intersect = Geometry2D.segment_intersects_segment(p1, p2, p1a, p2a)
+			if intersect != null: return true
+	return false
